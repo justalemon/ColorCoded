4382c1da91e3d7a37cbee9960e7b6c79305e37bb
Hannele Ruiz <justlemoncl@gmail.com>
2021-01-07 19:47:10 -0300

    Added basic script

---
+++ ColorCoded/Script.cs
@@ -0,0 +1,32 @@
+﻿using GTA;
+using System;
+
+namespace ColorCoded
+{
+    /// <summary>
+    /// Script that handle the DualShock 4 and DualSense color.
+    /// </summary>
+    public class ColorCoded : Script
+    {
+        #region Constructor
+
+        public ColorCoded()
+        {
+            Tick += ColorCoded_Tick;
+        }
+
+        #endregion
+
+        #region Functions
+
+        #endregion
+
+        #region Events
+
+        private void ColorCoded_Tick(object sender, EventArgs e)
+        {
+        }
+
+        #endregion
+    }
+}
